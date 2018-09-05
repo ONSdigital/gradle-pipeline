6cad12f7c18357d7bbcf0e31391a6b3372e5afc4
haqa <ameenhaq79@gmail.com>
2018-09-05 22:16:14 +0100

    [Gradle Release Plugin] - new version commit:  '2.7'.

--- release.sh
+++ release.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "Release in progress....."
-echo 'current version = ' $1
-echo 'bumping to = ' $2
-gradle release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion=$1 -Prelease.newVersion=$2
-echo "Successfully bumped version to $2"
+echo 'Current version: ' $1
+echo -n "Are you sure you wish to release with tag [$2]? (y/n) > "
+    read response
+    if [ "$response" != "y" ]; then
+        echo 'Bumping version to: ' $2
+        gradle release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion=$1 -Prelease.newVersion=$2
+        echo "Successfully bumped version to $2"
+        exit 1
+    fi
